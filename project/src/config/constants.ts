56472ddb1d62e8a7b56f1f91da1c72bce5b6153b
Amodit Jha <79688842+amoditjha@users.noreply.github.com>
2025-01-06 20:17:24 +0530

    Update constants.ts

--- project/src/config/constants.ts
+++ project/src/config/constants.ts
@@ -1,7 +1,7 @@
 export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5/weather';
 export const FORECAST_API_URL = 'https://api.openweathermap.org/data/2.5/forecast';
-export const API_KEY = '768b632f0a8c796e1a672a836e26a467';
-export const MAPBOX_TOKEN = 'pk.eyJ1Ijoic3RhY2tibGl0eiIsImEiOiJjbHRwOWV2Y2gwMXBqMmlvNGJ6NXBhNWVqIn0.YzGzKVEgr9RXuOTQFxb7Qw';
+export const API_KEY = 'add you oprnWeather api key here';
+export const MAPBOX_TOKEN = 'Add you mapbox token here';
 
 export const DAYS_OF_WEEK = [
   'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'
